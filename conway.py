88e5d21cb09f3a2dd650eb6f7f6380de8238c2e9
sarahdunbar <sarahdunbar@hanovernorwichschools.org>
2015-10-15 14:53:59 -0400

    Updated from Brython Server: 10/15/2015 2:53:59 PM

--- conway.py
+++ conway.py
@@ -6,3 +6,28 @@ Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
+class Cubey(Sprite):
+    def __init__(self, position):
+
+class Conway(App):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        life = Color(0xeeff00, 1.0)
+        norm = Color(0x872657, 1.0)
+        whiwhi = Color(0xFFFFFF, 1.0)
+        lifeline = LineStyle (1, life)
+        repline = LineStyle (1, norm)
+        whiteline = LineStyle (1, whiwhi)
+        deadark = RectangleAsset(5, 5, whiteline, whiwhi)
+
+    def step(self):
+        for cw in self.getSpritesbyClass(Cubey):
+            cw.step()
+
+
+myapp = Conway()
+myapp.run()
+
