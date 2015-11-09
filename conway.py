4a40addab701e328f345b141db5706d3b67cede6
sarahdunbar <sarahdunbar@hanovernorwichschools.org>
2015-11-09 14:50:34 -0500

    Updated from Brython Server: 11/9/2015 2:50:33 PM

--- conway.py
+++ conway.py
@@ -21,7 +21,7 @@ print ("By 207, the Miori-Ethrian war had reached new heights of hostility. The 
 " had first begun in 148, have largely been wiped out. Now, both the Miorians and Ethrians have turned to " + 
 "magical children as young as six years old to aid in their efforts. Drafting is not voluntary.")
 print (" ")
-print ("In this story, you will be playing the part of a novice teleporter as they rise through the ranks. Although young, you are " +
+print ("In this story, you will be playing the part of a novice teleporter as they rise through the ranks of the Miorian Army. Although young, you are " +
 "more than ready to fight and beat those dirty Ethrians back to where they belong. At least, you think you are.")
 print (" ")
 print (" ")
@@ -34,5 +34,33 @@ print (" ")
 print (" ")
 print (" ")
 print (" ")
-name = input ("First, you will need a name. Please enter your name: ")
-print ("Hello, " + name + ", 
+name = input ("Please select a name: ")
+age = input (name + ", how old are you? ")
+age = int (age)
+print (" ")
+g = 0
+while g == 0:
+    if age < 6:
+        age = input ("Even prodigies can't be that young. Please select again: ")
+        age = int (age)
+    elif age > 13 and age < 19: 
+        age = input ("You're a child, not a teenager. Please select again: ")
+        age = int (age)
+    elif age >= 19:
+        age = input ("You're a child, not an adult. Please select again: ")
+        age = int (age)
+    else:
+        print ("That will do. Now, time to become yourself.")
+        g = 1
+print (" ")
+
+
+
+
+
+
+
+
+
+
+
